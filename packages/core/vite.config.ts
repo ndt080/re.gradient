f7c4ac685409fa727f537148e33af10423fa9563
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2023-01-10 14:37:09 +0300

    init core package

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const srcPath = fileURLToPath(new URL("./src", import.meta.url));
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    target: "esnext",
+    minify: "terser",
+    lib: {
+      entry: "src/main.ts",
+      name: "darkvi-core",
+      formats: ["es", "umd", "cjs"],
+      fileName: (format) => `darkvi-core.${format}.js`,
+    },
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: srcPath }],
+  },
+  define: {
+    "process.env.NODE_ENV": '"production"',
+  },
+});
